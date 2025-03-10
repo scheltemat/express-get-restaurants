d02b7d4fc504ed0b9e3f8caef60cbf056845cafd
Scheltema <thomas.scheltema@hp.com>
2025-03-10 15:53:07 -0500

    part 2 is done

--- src/app.js
+++ src/app.js
@@ -4,9 +4,13 @@ const { Restaurant } = require("../models/index");
 const { db } = require("../db/connection");
 
 app.get("/restaurants", async (req, res) => {
-  await db.sync();
   const restaurants = await Restaurant.findAll();
   res.json(restaurants);
+});
+
+app.get("/restaurants/:id", async (req, res) => {
+  const restaurant = await Restaurant.findByPk(req.params.id);
+  res.json(restaurant);
 });
 
 module.exports = app;
