19f0e4b17cf22f003a872adfe3bad828c4a827c2
Scheltema <thomas.scheltema@hp.com>
2025-03-10 16:09:46 -0500

    part 3 done

--- src/app.js
+++ src/app.js
@@ -3,6 +3,9 @@ const app = express();
 const { Restaurant } = require("../models/index");
 const { db } = require("../db/connection");
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/restaurants", async (req, res) => {
   const restaurants = await Restaurant.findAll();
   res.json(restaurants);
@@ -11,6 +14,43 @@ app.get("/restaurants", async (req, res) => {
 app.get("/restaurants/:id", async (req, res) => {
   const restaurant = await Restaurant.findByPk(req.params.id);
   res.json(restaurant);
+});
+
+app.post("/restaurants", async (req, res) => {
+  try {
+    const newRestaurant = await Restaurant.create(req.body);
+    res.status(201).json(newRestaurant);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+app.put("/restaurants/:id", async (req, res) => {
+  try {
+    const restaurant = await Restaurant.findByPk(req.params.id);
+    if (restaurant) {
+      await restaurant.update(req.body);
+      res.json(restaurant);
+    } else {
+      res.status(404).json({ message: "Restaurant not found" });
+    }
+  } catch (err) {
+    res.status(500).json({ message: err.message })
+  }
+});
+
+app.delete("/restaurants/:id", async (req, res) => {
+  try {
+    const restaurant = await Restaurant.findByPk(req.params.id);
+    if (restaurant) {
+      await restaurant.destroy();
+      res.status(204).send();
+    } else {
+      res.status(404).json({ message: "Restaurant not found" });
+    }
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 });
 
 module.exports = app;
